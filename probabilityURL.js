acc5f0eafd6a27eaa2adb6908bd9210e10f02213
minntelia <99382065+minntogames@users.noreply.github.com>
2022-10-13 14:25:41 +0900

    Create probabilityURL.js

---
+++ probabilityURL.js
@@ -0,0 +1,18 @@
+//通常リンクのURLを指定する
+	my_url1 ='https://minntogames.github.io/index.html';
+	//通常リンクの文字を指定する
+	my_name1='普通のリンク';
+
+	//ランダムリンクのURLを指定する
+	my_url2 ='https://minntogames.github.io/128371631872375736812.html';
+	//ランダムリンクの文字を指定する
+	my_name2='気まぐれリンク';
+
+	//リンクが表示する確率を指定する
+	//大きいほど確率は低く、小さいほど高くなります
+	ran=15;
+
+	if(Math.floor(Math.random()*ran) < 2)
+		document.write('<a href="'+my_url2+'">'+my_name2+'</a><br>');
+	else 
+		document.write('<a href="'+my_url1+'">'+my_name1+'</a><br>');
