e78139f872b45df62d1cbc8bafca1f4d6fa06d54
minntelia <99382065+minntogames@users.noreply.github.com>
2022-10-13 14:32:09 +0900

    Update probabilityURL.js

--- probabilityURL.js
+++ probabilityURL.js
@@ -1,12 +1,12 @@
 //通常リンクのURLを指定する
 	my_url1 ='https://minntogames.github.io/index.html';
 	//通常リンクの文字を指定する
-	my_name1='普通のリンク';
+	my_name1='Minnteliaのお部屋';
 
 	//ランダムリンクのURLを指定する
 	my_url2 ='https://minntogames.github.io/128371631872375736812.html';
 	//ランダムリンクの文字を指定する
-	my_name2='気まぐれリンク';
+	my_name2='MINNTERIAのお部屋';
 
 	//リンクが表示する確率を指定する
 	//大きいほど確率は低く、小さいほど高くなります
