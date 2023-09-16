d76b98d1996b05c85184cb54e167f3661c43e685
minntelia <99382065+minntogames@users.noreply.github.com>
2023-09-16 09:36:37 +0900

    固定下げ

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("scroll", function() {
     const header = document.querySelector(".fixed-header");
-    if (window.scrollY > 100) { // スクロール位置が100pxを超えたら表示
+    if (window.scrollY > 150) { // スクロール位置が150pxを超えたら表示
         header.style.top = "0";
     } else {
-        header.style.top = "-100px"; // スクロール位置が100px未満の場合は非表示
+        header.style.top = "-150px"; // スクロール位置が150px未満の場合は非表示
     }
 });
