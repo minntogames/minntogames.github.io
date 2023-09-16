34154eb6d09b341f4b8e38174cf056c9f3fc0b59
minntelia <99382065+minntogames@users.noreply.github.com>
2023-09-16 18:49:37 +0900

    自動調整

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("scroll", function() {
     const header = document.querySelector(".fixed-header");
-    if (window.scrollY > 150) { // スクロール位置が150pxを超えたら表示
+    if (window.scrollY > 150) { // スクロール位置が100pxを超えたら表示
         header.style.top = "0";
     } else {
-        header.style.top = "-150px"; // スクロール位置が150px未満の場合は非表示
+        header.style.top = "-150px"; // スクロール位置が100px未満の場合は非表示
     }
 });
 
@@ -11,28 +11,19 @@ function resizeIframe() {
     const iframe = document.getElementById('youtube-iframe');
     const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-    // サイズの上限と下限を設定
-    const minWidth = 100; // 最小幅
-    const maxWidth = 400; // 最大幅
+    // サイズの下限を設定
+    const maxWidth = 500; // 最大幅
 
-    // ウィンドウの幅に合わせて iframe の幅を設定
-    let newWidth = windowWidth;
-
-    // サイズの下限をチェック
-    if (newWidth < minWidth) {
-        newWidth = minWidth;
+    if (windowWidth > maxWidth) { // ウィンドウの幅が最大幅を超える場合は最大幅に設定
+        iframe.style.width = 400 + 'px';
+    } else {
+        // ウィンドウの幅に合わせて iframe の幅を設定
+        iframe.style.width = windowWidth - 100 + 'px';
     }
-
-    // サイズの上限をチェック
-    if (newWidth > maxWidth) {
-        newWidth = maxWidth;
-    }
-
-    iframe.style.width = newWidth + 'px';
 
     // 高さを計算して設定
     const aspectRatio = 16 / 9;
-    const iframeHeight = newWidth / aspectRatio;
+    const iframeHeight = parseInt(iframe.style.width) / aspectRatio;
     iframe.style.height = iframeHeight + 'px';
 }
 
