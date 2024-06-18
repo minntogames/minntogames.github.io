99042771d8914fbf113df12aa762e9e4776218e0
minntelia <99382065+minntogames@users.noreply.github.com>
2024-06-19 03:07:56 +0900

    pa

--- java/script.js
+++ java/script.js
@@ -27,6 +27,22 @@ function resizeIframe() {
     iframe.style.height = iframeHeight + 'px';
 }
 
+document.getElementById('videoContainer').addEventListener('click', function() {
+    const videoContainer = this;
+    const videoID = 'SBCJj6tQDYU'; // YouTubeの動画IDをここに入力
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('width', '560');
+    iframe.setAttribute('height', '315');
+    iframe.setAttribute('src', `https://www.youtube.com/embed/${videoID}?autoplay=1`);
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+    iframe.setAttribute('allowfullscreen', 'true');
+
+    // クリック後に埋め込みを表示するためにコンテナの内容をクリアしてiframeを追加
+    videoContainer.innerHTML = '';
+    videoContainer.appendChild(iframe);
+});
+
 // 初期ロード時とウィンドウリサイズ時に iframe を調整
 window.addEventListener('load', resizeIframe);
 window.addEventListener('resize', resizeIframe);
