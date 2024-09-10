ce116ec7981186d3ebb22298c0116bef922f2444
Minntelia <takatakabatu@gmail.com>
2024-09-11 04:46:15 +0900

    修正変更追加諸々

--- java/script.js
+++ java/script.js
@@ -6,3 +6,17 @@ document.addEventListener("scroll", function() {
         header.style.top = "-150px"; // スクロール位置が100px未満の場合は非表示
     }
 });
+
+//ハンバーガーメニュー
+window.onload = function () {
+    var nav = document.getElementById('nav-wrapper');
+    var hamburger = document.getElementById('js-hamburger');
+    var blackBg = document.getElementById('js-black-bg');
+  
+    hamburger.addEventListener('click', function () {
+        nav.classList.toggle('open');
+    });
+    blackBg.addEventListener('click', function () {
+        nav.classList.remove('open');
+    });
+  };
