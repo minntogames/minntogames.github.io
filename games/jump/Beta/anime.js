dcb89ee0f1030c5f05330bb4e4e09d5096b4f1a7
Minntelia <takatakabatu@gmail.com>
2025-07-23 21:27:06 +0900

    asdasdasasdas

--- games/jump/Beta/anime.js
+++ games/jump/Beta/anime.js
@@ -1201,7 +1201,7 @@ function animate() {
 // Google Sheetにスコアを送信する関数
 function sendScoreToGoogleSheet(altitude, userId) {
     // TODO: ここにデプロイしたGoogle Apps ScriptのウェブアプリのURLを設定してください
-    const gasWebAppUrl = 'https://script.google.com/macros/s/AKfycbwL4iio-mGRRQRcIcHmtPuPvN1O_Kho2m_Nhm_QsWn-Y3oVyOWzIrPfYuYkSSD20iXV/exec'; // ここをデプロイしたGASウェブアプリのURLに置き換えてください！
+    const gasWebAppUrl = 'https://script.google.com/macros/s/AKfycbz_G_AXV5enLCBFh1oI6gFPYP_3pNSNN3cG6t1CwkRh-tadL0ZS8JsK-8m06UOhBhso/exec'; // ここをデプロイしたGASウェブアプリのURLに置き換えてください！
 
     if (gasWebAppUrl === 'YOUR_DEPLOYED_GAS_WEB_APP_URL_HERE') {
         console.warn("Google Apps ScriptのウェブアプリURLが設定されていません。データを送信できません。");
